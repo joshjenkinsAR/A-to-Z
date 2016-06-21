ac6b9885517388d4bff855eccda4947db4731941
Josh Jenkins <joshjenkinsAR@gmail.com>
2016-06-21 15:47:57 -0500

    Shortcode update

---
+++ includes/az-shortcode.php
@@ -0,0 +1,51 @@
+<?php
+function az_shortcode($atts){
+
+//Call the stylesheet if the shortcode is present
+//wp_register_script( 'proglist-filter', plugins_url('list.min.js', __FILE__), array('jquery'), true );
+//wp_register_script( 'proglist-init', plugins_url('init.js', __FILE__), array('dirswitch-filter'), true );
+
+$terms = get_terms( 'alpha' );
+ ?>
+       <div id="az-index">
+
+		   
+			<?php 	
+			foreach ( $terms as $term ) {
+			
+			
+			
+			$options = array(
+				'post_type' => 'item',
+				'orderby' => 'title',
+				'order'   => 'ASC',
+				'tax_query' => array (
+							array (
+								'taxonomy' => 'alpha',
+								'field' => array($term->slug),
+								'terms' => array($term->term_id),
+							),
+						),
+			);	
+			
+	$query = new WP_Query( $options );
+    // run the loop based on the query
+    if ( $query->have_posts() ) { 
+			echo '<div class="'. $term->name .'"><div id="letter-heading">' . $term->name .'</div>';
+			while ( $query->have_posts() ) : $query->the_post();
+			 ?>
+			<li class="plain-list switch-view"><a href="<?php the_permalink();?>" class="item-title"><?php the_title(); ?></a>
+			<div class="hidden-card">
+			<div class="item-description"><?php echo get_post_meta( get_the_ID(), 'item_description', true ); ?></div>
+			</div>
+			</li>
+			
+            <?php 
+			endwhile;
+			echo '</div>';
+			}  
+    }
+	
+	echo '</div>';	
+}
+add_shortcode('az-index','az_shortcode');
